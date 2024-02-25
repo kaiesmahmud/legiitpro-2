3efd3e4320cd5fe54d25204e993800819389e74f
kaiesmahmud <kaiesmahmud@gmail.com>
2024-02-25 20:18:27 +0600

    sub-cat server setup done

--- src/routes/[topic]/[subcategory]/+page.server.js
+++ src/routes/[topic]/[subcategory]/+page.server.js
@@ -1,24 +1,26 @@
+import { redirect } from '@sveltejs/kit'
+import { sub_category_page_data } from '../../../lib/constant/sub_category_page_data.js'
+
 export const load= async(events)=>{
     // console.log(events)
     let  {url,params } = events
     // console.log(params.topic)
     // console.log(url.pathname)
-    //let backendData = category_page_data.filter(i=> i.url === url.pathname)[0]
+    let backendData = sub_category_page_data.filter(i=> i.subCatURL === url.pathname)[0]
     // console.log(backendData)
-    // if(backendData){
-    //     let {pageName , url, frontend,subCategories} = backendData
-    //     // console.log(frontend)
-    //     // console.log(categories)
-    //     return{
-    //         pageName:pageName , 
-    //         url:url,
-    //         frontend:frontend,
-    //         subCategories:subCategories
-    //         }
-    // }else{
-    //     console.log("Not Found Data")
-    //     redirect(302, '/global-seo/'); // Redirect to the parent page
-    // }
+    if(backendData){
+        let {subCatName , subCatURL, frontend, } = backendData
+        // console.log(frontend)
+        // console.log(categories)
+        return{
+            subCatName:subCatName , 
+            subCatURL:subCatURL,
+            frontend:frontend, 
+            }
+    }else{
+        console.log("Not Found Data")
+        redirect(302, '/'); // Redirect to the parent page
+    }
     return{ url : params.subcategory}
     // return{ url : url.pathname}
 }

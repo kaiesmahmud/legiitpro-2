c7447d2b18df1c6f3077fc369bd9c54b32fd2d2c
kaiesmahmud <kaiesmahmud@gmail.com>
2024-03-06 12:02:51 +0600

    updt

--- src/lib/components/thrive-components/ThriveServices.svelte
+++ src/lib/components/thrive-components/ThriveServices.svelte
@@ -7,7 +7,7 @@
 <div class="flex flex-col items-center justify-center">
     <section class="w-full md:w-[80%] py-10">
         <div class="flex items-center flex-col justify-center text-center gap-2 md:gap-5 py-3">
-            <h3 class="text-3xl md:text-4xl lg:text-5xl font-extrabold ">{title}</h3>
+            <h3 class="text-3xl md:text-4xl lg:text-5xl font-extrabold ">{@html title}</h3>
             {#if subTitle}
             <p class="p-3 md:p-5 text-black/70 ">{subTitle}</p>
             {/if}
@@ -20,7 +20,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:p-5">
             {#each services as {service_description,service_title}}
                  <div class="flex flex-col   gap-3 p-5 rounded-xl bg-indigo-500/10">
-                    <h5 class="text-2xl font-bold  text-slate-900">{service_title}</h5>
+                    <h5 class="text-2xl font-bold  text-slate-900">{@html service_title}</h5>
                     <p class="text-slate-900/70 p-3 ">{@html service_description}</p>
                  </div>
             {/each}

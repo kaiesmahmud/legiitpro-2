d3880f719213ed48574c86e62b43abd7cf7ced00
kaiesmahmud <kaiesmahmud@gmail.com>
2024-03-06 11:28:22 +0600

    updt

--- src/lib/constant/servicesData.js
+++ src/lib/constant/servicesData.js
@@ -6,17 +6,17 @@ export const serviceSectionData = [
         cat:[
             {
                 catName: "Social Media Management" ,
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Our digital marketing firm studies your objectives, benchmarks your competitors, and looks at how your clients behave online. We create specialized paid advertising and social media brand management plans for your company based on data and analytics.",
                 catURL : "/b2b-marketing-agency/social-media-management" ,
             },
             {
                 catName: "Digital Advertising" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "The process of social media advertising can be intimidating, particularly if you lack the experience, know-how, and abilities necessary to assist with the planning, execution, evaluation, and optimization of sponsored social media advertising campaigns. Searching for a trustworthy company to handle your social media advertising?",
                 catURL : "/b2b-marketing-agency/digital-advertising" ,
             },
             {
                 catName: "Lead Management" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Lead management services assist your client in gathering information from prospective clients, monitoring their actions, and converting them into paying clients.  It increases sales like an extremely potent contact list!",
                 catURL : "/b2b-marketing-agency/b2b-lead-generation" ,
             },
         ],
@@ -28,22 +28,22 @@ export const serviceSectionData = [
         cat:[
             {
                 catName: "CMS SEO" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "CMS lets you create and manage website content, while SEO helps that content rank higher in search results. Imagine CMS as the engine room and SEO as the fuel that drives website traffic.",
                 catURL : "/b2b-marketing-agency/cms-seo" 
             },
             {
                 catName: "ISW SEO" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "ISW SEO, or International Subwebsite SEO, refers to optimizing your website's international subdomains for specific languages and regions. This ensures your website ranks well in search engines for relevant searches in each target market.",
                 catURL : "/b2b-marketing-agency/isw-seo" 
             },
             {
                 catName: "eCommerce SEO" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Boost customer retention by offering your clients a convenient, round-the-clock purchasing experience. In order to generate individualized brand experiences, we run focused email marketing campaigns and optimize your website for voice search and mobile devices. Integrations with Walmart Marketplace are another service we provide to boost your sales volume.",
                 catURL : "/b2b-marketing-agency/ecommerce-seo" 
             },
             {
                 catName: "Others SEO" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Boost your online presence with comprehensive SEO services! These services encompass everything from keyword research and content optimization to technical adjustments and link building, helping your website rank higher in search engine results, attract more organic traffic, and ultimately drive conversions.",
                 catURL : "/b2b-marketing-agency/others-seo" 
             },
         ],
@@ -55,17 +55,17 @@ export const serviceSectionData = [
         cat:[
             {
                 catName: "ISW Development" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Go global with international subwebsites! These tailored versions of your website speak to different languages and regions, attracting more visitors, boosting search rankings, and enhancing user experience. It's like having multiple shopfronts, each catering to specific cultures and needs.",
                 catURL : "/b2b-marketing-agency/international-sub-website-development" 
             },
             {
                 catName: "website Integration" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "Integrating your website with other platforms and technologies is known as website integration. Allowing data to move effortlessly between your website and, say, your payment system, email marketing tool, or social networking accounts is similar to constructing bridges. This improves productivity, automates processes, and builds a stronger, more effective online presence.",
                 catURL : "/b2b-marketing-agency/website-integration" 
             },
             {
                 catName: "eCommerce Development" , 
-                catDesc: "We manage every aspect of your social media- from content strategy, branding and images, to community engagement. We research trends and news to create content that keeps your brand current and relevant.",
+                catDesc: "The creation of an e-commerce website puts your shop online! It entails developing an intuitive platform to organize purchases, display items, and take safe payments. Imagine creating a virtual storefront that is always accessible for customers to peruse and make purchases.",
                 catURL : "/b2b-marketing-agency/ecommerce-website-development" 
             },
         ],

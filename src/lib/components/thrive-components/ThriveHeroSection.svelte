a85c56e6a6b8cc6c4e6874617daeb2d2ca605029
kaiesmahmud <kaiesmahmud@gmail.com>
2024-03-17 13:29:40 +0600

    Youtube video testing

--- src/lib/components/thrive-components/ThriveHeroSection.svelte
+++ src/lib/components/thrive-components/ThriveHeroSection.svelte
@@ -22,4 +22,7 @@
              transition-all ease-in`}>Get My Free Proposal</a>
         </div>
     </section>
+    <section class="w-full md:w-[90%] lg:w-[70%] rounded-xl overflow-hidden my-5 h-auto">
+        <iframe class="w-full" width="560" height="515" src="https://www.youtube.com/embed/gztJj-Y96DY?si=5QW4h9qPYZW1Pmx2&autoplay=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>
 </div>
